fe2566ae6c1f3bb92501f858acea232f4c0cc51d
Mashakuzmichevaaa <113968572+Mashakuzmichevaaa@users.noreply.github.com>
2022-11-03 00:13:32 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
+
 void Mass(int n, double* coordinates) {
   double k;
   for (int i = 0; i < n; i++) {
@@ -8,12 +9,13 @@ void Mass(int n, double* coordinates) {
     if (i == 0) {
       coordinates[i] = k;
     } else {
-      double c = pow(coordinates[i - 1], i / (1.0 + i));
-      double d = pow(k, 1 / (1.0 + i));
-      coordinates[i] = c * d;
+      double coord1 = pow(coordinates[i - 1], i / (1.0 + i));
+      double coord2 = pow(k, 1 / (1.0 + i));
+      coordinates[i] = coord1 * coord2;
     }
   }
 }
+
 int main() {
   int n;
   std::cin >> n;
@@ -24,14 +26,14 @@ int main() {
   std::cin >> q;
   for (int i = 0; i < q; i++) {
     std::cin >> left >> right;
-    double a;
+    double coord;
     if (left == 0) {
-      a = coordinates[right];
+      coord = coordinates[right];
     } else {
-      a = coordinates[right] / pow(coordinates[left - 1], left / (right + 1.0));
+      coord = coordinates[right] / pow(coordinates[left - 1], left / (right + 1.0));
     }
-    double b = (right + 1) / (right - left + 1.0);
-    double answer = pow(a, b);
+    double degree = (right + 1) / (right - left + 1.0);
+    double answer = pow(coord, degree);
     std::cout << answer << std::endl;
   }
   delete[] coordinates;
